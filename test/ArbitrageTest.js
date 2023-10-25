a091e7476cf692214919967e5782e88cb66d804d
CyberAnon <juniorprimeau@outlook.com>
2023-10-25 10:10:49 -0400

    6. Arbitrage Test Script

---
+++ test/ArbitrageTest.js
@@ -0,0 +1,44 @@
+const { expect } = require("chai")
+
+const config = require("../config.json")
+
+describe("Arbitrage", () => {
+  let owner
+  let arbitrage
+
+  beforeEach(async () => {
+    [owner] = await ethers.getSigners()
+
+    arbitrage = await hre.ethers.deployContract(
+      "Arbitrage",
+      [
+        config.SUSHISWAP.V2_ROUTER_02_ADDRESS,
+        config.UNISWAP.V2_ROUTER_02_ADDRESS
+      ]
+    )
+
+    await arbitrage.waitForDeployment()
+  })
+
+  describe("Deployment", () => {
+    it("Sets the sRouter", async () => {
+      expect(await arbitrage.sRouter()).to.equal(config.SUSHISWAP.V2_ROUTER_02_ADDRESS)
+    })
+
+    it("Sets the uRouter", async () => {
+      expect(await arbitrage.uRouter()).to.equal(config.UNISWAP.V2_ROUTER_02_ADDRESS)
+    })
+
+    it("Sets the owner", async () => {
+      expect(await arbitrage.owner()).to.equal(await owner.getAddress())
+    })
+  })
+
+  describe("Trading", () => {
+
+    /**
+     * Feel Free to customize and add in your own unit testing here.
+     */
+
+  })
+})
